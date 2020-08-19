37d74279282aacb4464a66ccf5963a37cde47dd3
Sergey Rodin <rodin.s@rambler.ru>
2020-08-19 09:30:46 +0300

    CityInfoFragmentTest.kt created.

---
+++ app/src/androidTest/java/com/sergeyrodin/citiestask/info/CityInfoFragmentTest.kt
@@ -0,0 +1,10 @@
+package com.sergeyrodin.citiestask.info
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CityInfoFragmentTest {
+
+}
